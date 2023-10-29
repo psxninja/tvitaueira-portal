20fa1efda5e5d9ef4bdfa3fee1ef21d874c41aa8
psxninja <pablo270santos@gmail.com>
2023-10-29 01:15:30 -0300

    fix cache routes

--- app/api/cache/all/route.ts
+++ app/api/cache/all/route.ts
@@ -1,13 +1,6 @@
 import { revalidatePath } from 'next/cache'
-import { NextRequest } from 'next/server'
 
-export async function GET(
-	req: NextRequest,
-	{ params }: { params: { postslug: string } }
-) {
-	if (!params.postslug) {
-		return new Response(JSON.stringify({ code: '2' }), { status: 400 })
-	}
+export async function GET() {
 	revalidatePath('/', 'layout')
 	return new Response(JSON.stringify({ code: '1' }))
 }
--- app/api/user/route.ts
+++ app/api/user/route.ts
@@ -1,8 +1,7 @@
-import { NextRequest } from 'next/server'
 import bycript from 'bcryptjs'
 
-export async function GET(req: NextRequest) {
-    const hashedPassword = await bycript.hash('', 10)
+export async function GET() {
+	const hashedPassword = await bycript.hash('', 10)
 
 	return new Response(JSON.stringify({ pass: hashedPassword }))
 }
